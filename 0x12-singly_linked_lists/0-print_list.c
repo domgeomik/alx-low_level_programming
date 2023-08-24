499019f8f169b65ada74a3dd5cea9080d039da25
domgeomik <domgeomik@gmail.com>
2023-08-24 08:09:08 -0700

    0x12. C - Singly linked lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include "lists.h"
 /**
-* print_list - prints all List Linked elements 
+* print_list - Prints all the Elements of a linked list
 * @h: pointer to the list_t list to print
-* Return: the number of nodes printed
+*
+* Return: the Number of Nodes printed
 */
 size_t print_list(const list_t *h)
 {
